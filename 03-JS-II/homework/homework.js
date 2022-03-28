4ca37e4d6543f2c19a3703c08455520ca4b1bc69
julianluissoto <julianlasoto@hotmail.com>
2022-03-28 15:22:05 -0300

    homework tarea 3 resuelto

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -175,7 +175,7 @@ function operadoresLogicos(num1, num2, num3) {
   if (num1 < 0 || num2 < 0 || num3 < 0) {
     return "Hay negativos";
   }
-  if (num3 > num1 && num3 > num2) {
+  if (num3 > num1 && num3 > num2 && num1 > 0 && num2 > 0 && num3 > 0) {
     num3++;
     return num3;
   }
